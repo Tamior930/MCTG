1e405b2450ab8aac741b1779e9ac544476cbf6fa
if23b297 <Tamior@DESKTOP-6C2U1IA>
2024-10-03 21:26:13 +0200

    Add project files.

---
+++ MCTG/Models/User.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MCTG.Models
+{
+    internal class User
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Token { get; set; }
+        public int Coins { get; set; } = 20;
+        // Deck
+        public int ELO { get; set; } = 100;
+
+        public User(string username, string password)
+        {
+            Username = username;
+            Password = password;
+            Token = null;
+        }
+
+    }
+}
